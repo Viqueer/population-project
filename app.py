8a9738a5f165c281604c0393497b3278fbcf3a79
Egbadon Victor <thethinkerx100@gmail.com>
2022-01-13 07:45:32 +0100

    second commit

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from pymongo import MongoClient
 import pandas as pd
 import numpy as np
 from plots import *
+import json
+import matplotlib.pyplot as plt
+import statistics
 
 app =  Flask(__name__)
 
@@ -14,25 +17,44 @@ def index():
 @app.route('/refresh_plots', methods=['GET'])
 def refresh_plots():
 
-    #connect to db
-    CONNECTION_STRING = "mongodb+srv://owolabioromidayo:thisisdayo99@cluster0.28vgr.mongodb.net/gdp_country?retryWrites=true&w=majority"
-    client = MongoClient(CONNECTION_STRING)
-    collection = client['gdp_country']['gdp_country']
-    cursor = collection.find()
+   
     
     #get and mutate dataframe 
-    df = pd.DataFrame(list(cursor))
-    df = df.set_index('Reference Area')
-    del df['_id']
-    df['Time Period'] = pd.to_numeric(df['Time Period'])
-    df['Observation Value'] = pd.to_numeric(df['Observation Value'])
-    df = df.sort_values('Observation Value')
-    print(df)
+    with open('yearsdata.json') as json_file:
+        yearsdata = json.load(json_file)
+    with open('countriesdata.json') as json_file2:
+        countriesdata = json.load(json_file2)
+    # df = df.set_index('Reference Area')
+    # del df['_id']
+    # df['Time Period'] = pd.to_numeric(df['Time Period'])
+    # df['Observation Value'] = pd.to_numeric(df['Observation Value'])
+    # df = df.sort_values('Observation Value')
+    # print(df)
+    count = 0
+    for country, payload in countriesdata.items():
     
-    #generate plots
-    bar_plots(df)
-    dispersion_plots(df)
-    box_plots(df)
+        plt.title(country)
+        plt.ylabel("Female to male population ratio")
+        plt.xlabel("Years")
+        years =[] 
+        datas = []
+        for year, data in payload.items(): 
+            if year == "Country Code" or year == "Color":
+                break
+            years.append(year)
+            counterdata = 100 - data
+            datas.append(data/counterdata)
+        x = np.array(years)
+        y = np.array(datas)
+        plt.bar(x,y, color = countriesdata[country]["Color"])
+        plt.savefig(f'./static/images/hbars3_{count}.png')
+        plt.show()
+        count+=1
+    
+    # #generate plots
+    # bar_plots(df)
+    # dispersion_plots(df)
+    # box_plots(df)
 
     return redirect("/")
 
