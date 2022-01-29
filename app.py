62e5285f864ebe8a5758c21b1619e1a56db74a5e
Egbadon Victor <thethinkerx100@gmail.com>
2022-01-29 16:22:22 +0100

    Revert "included the redis database"
    This reverts commit fe3f08ef75d6dceb6f913b545b9f84a2a90d43ce.

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ from plots import *
 import json
 import matplotlib.pyplot as plt
 import statistics
-import csv
-from optparse import OptionParser
-import redis
-import sys
 
 app =  Flask(__name__)
 
@@ -61,52 +57,6 @@ def refresh_plots():
     # box_plots(df)
 
     return redirect("/")
-
-
-
-
-
-#!/usr/bin/env python
-'''
-:mod:`csv2redis.csv2redis` is a module containing classes for importing CSV files to Redis.
-'''
-
-
-
-
-def import_csv(key_col=1, csv_file="yearsdata.csv", redis_host='egbadon.redis.cache.windows.net', redis_port=6379, azurePassword='HHDI4wBkIcTgmCSvZD0nxgxaPvZw2iYEtAzCaG9897c='):
-    pool = redis.ConnectionPool(host=redis_host, port=redis_port, db=0, password=azurePassword)
-    r = redis.Redis(connection_pool=pool)
-    pipe = r.pipeline()
-    with open(csv_file, 'r') as cf:
-        cfreader = csv.reader(cf)
-        headers = cfreader.__next__()
-        for row in cfreader:
-            record = {}
-            for h, v in zip(headers, row):
-                record[h] = v
-            pipe.hmset(row[key_col], record)
-    pipe.execute()
-
-
-if __name__ == "__main__":
-    usage = "usage: %prog [options]"
-    parser = OptionParser(usage)
-    parser.add_option("-f", "--file", dest="csvfile", help="CSV file to import", metavar="FILE")
-    parser.add_option("-n", "--name-column", dest="namecolumn", help="Column number to use as name")
-    parser.add_option("-r", "--redis-host", dest="redishost", help="Redis host name", default="localhost")
-    parser.add_option("-p", "--redis-port", dest="redisport", help="Redis port", default=6379)
-
-    (options, args) = parser.parse_args()
-
-    # if options.csvfile is None:
-    #     parser.error("Missing reqiured option csv file")
-    #
-    # if options.namecolumn is None:
-    #     parser.error("Missing required option name column")
-
-    # import_csv(int(options.namecolumn), options.csvfile, options.redishost, options.redisport)
-    import_csv()
 
 
 
